b2fa1814a5048b13cfee62b435e93111957ee835
Anqi Lu <anqi0103@gmail.com>
2021-06-04 21:14:10 -0700

    Integrate microservice

--- cat-scraper/server/index.js
+++ cat-scraper/server/index.js
@@ -84,6 +84,38 @@ app.post('/upload', upload.single('file'), (req, res) => {
   });
 });
 
+// Here is the example that how I scrape bird image from Wikipedia for my teammate
+app.get('/scraped/:target', (req, res) => {
+  axios('https://en.wikipedia.org/wiki/' + req.params["target"])
+      .then(response => {
+          const html = response.data;
+          const $ = cheerio.load(html);
+          const allImgTags = $('img');
+
+          const allUrls = [];
+
+          for (let i = 0; i < allImgTags.length; i++) {
+              allUrls.push(allImgTags[i].attribs.src);
+          }
+
+          res.status(200).send(allUrls);
+      })
+      .catch(console.error);
+});
+
+// Here is how I will use data from my teammates
+app.get('/homeCat', (req, res) => {
+  axios('http://flip2.engr.oregonstate.edu:7749/?search_name=cats')
+      .then(response => {
+          const data = response.data;
+          const arr = [];
+          for (const key of Object.keys(data)) {
+              arr.push(data[key]);
+          }
+          res.status(200).send(arr);
+      })
+      .catch(console.error);
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
